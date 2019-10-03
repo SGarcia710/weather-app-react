c62769ba4ad82994dd3da7b859a6d2aec4ef3d5b
Sebastián <sgarcia710@outlook.com>
2019-10-02 19:08:38 -0500

    all solved and app completed

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
 
   const getData = async () => {
     const response = await fetch(
-      `https://community-open-weather-map.p.rapidapi.com/weather?id=2172797&lang=sp&units=metric&mode=xml%2C%20html&q=${query.city}%2C${query.countryCode}`,
+      `https://community-open-weather-map.p.rapidapi.com/weather?id=2172797&lang=en&units=metric&mode=xml%2C%20html&q=${query.city}%2C${query.countryCode}`,
       {
         method: 'GET',
         headers: {
@@ -29,13 +29,12 @@ const App = () => {
           'x-rapidapi-key': '50b348b789mshc5e5fc6ede0488cp1b6fe1jsne7b277a7800b'
         }
       }
-    );
-    console.log(response);
+    ).catch();
     // Free OpenWeatherAPI account Sgarcia710 - 60 calls per minute
     //`https://api.openweathermap.org/data/2.5/weather?q=${query.city},${query.countryCode}&appid=801685f9e6f8de87e464e193e808b4ed`
     const json = await response.json();
-    console.log(json);
     if (json.cod === 200) {
+      // City found
       const {
         cod,
         main: { temp, humidity },
@@ -53,14 +52,13 @@ const App = () => {
         weatherDesc: weather[0].description,
         windSpeed: speed
       };
-      console.log(cod);
       setWeatherInfo(object);
     } else {
+      // City not found
       const { cod } = json;
       const object = {
         code: cod
       };
-      console.log(object);
       setWeatherInfo(object);
     }
   };
@@ -85,7 +83,7 @@ const App = () => {
             temp={weatherInfo.temp}
             humidity={weatherInfo.humidity}
             name={weatherInfo.name}
-            country={weatherInfo.countyr}
+            country={weatherInfo.country}
             weatherDesc={weatherInfo.weatherDesc}
             windSpeed={weatherInfo.windSpeed}
           />
--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -12,16 +12,29 @@ const WeatherInfo = ({
   code === 200 ? (
     <div className='card mb-3'>
       <div className='card-body'>
-        <h1 id='weather-location' className='h3'></h1>
-        <h3 id='weather-description' className='h4'></h3>
-        <h3 id='weather-string'></h3>
+        <h1 id='weather-location' className='h3'>
+          {name} / {country}
+        </h1>
+        <h3 id='weather-description' className='h4'>
+          {weatherDesc}
+        </h3>
+        <h3 id='weather-string'>{temp}°C</h3>
         <ul className='list-group mt-3'>
-          <li id='weather-humidity' className='list-group-item'></li>
-          <li id='weather-wind' className='list-group-item'></li>
+          <li id='weather-humidity' className='list-group-item'>
+            Humidity: {humidity}°
+          </li>
+          <li id='weather-wind' className='list-group-item'>
+            Wind speed: {windSpeed}m/s
+          </li>
         </ul>
       </div>
     </div>
   ) : (
-    <h1>Wrong search</h1>
+    <div className='card mb-3'>
+      <div className='card-body'>
+        <h1 className='h3'>Wrong search</h1>
+        <h6 className='h6'>Try again</h6>
+      </div>
+    </div>
   );
 export default WeatherInfo;
